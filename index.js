6fbe8eaafcc07d6249049873f1ba69a1028ac575
Romain <[r.cappellotti@gmail.com]>
2025-03-26 16:00:12 +1100

    Improving code and deleting css file

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
 import http from 'node:http';
 import fs from 'node:fs';
-import url from 'node:url';
+
 
 const server = http.createServer((req, res) => {
-    const getUrl = url.parse(req.url, true);
-    const pathName = getUrl.pathname;
 
-    if (pathName === '/') {
-        fs.readFile('index.html', (err, data) => {
-            if(err) {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                return res.end('Error Loading File');
-            };
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            return res.end(data)
-        });
-    } else if (pathName === '/about') {
-        fs.readFile('./about.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                return res.end('Error Loading File');
-            };
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            return res.end(data);
-        });
-    } else if (pathName === "/contact-me") {
-        fs.readFile('./contact-me.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                return res.end('Error Loading File');
-            }
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            return res.end(data);
-        });
-    } else {
-        fs.readFile('404.html', (err, data) => {
-            if (err) {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                return res.end('Error Loading File')
-            };
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            return res.end(data);
-        });
-    };
+    const myURL = new URL(req.url, 'http://localhost:8080/')
+    const pathName = myURL.pathname;
+    let fileName = '.' + pathName + '.html';
+
+    if (pathName === '/') fileName = './index.html';
+
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            fs.readFile('./404.html', (err404, data404) => {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                if(err404) {
+                    return res.end('Error 404, This localhost page cant be found')
+                }
+                return res.end(data404);
+            })
+            return;
+        }
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        return res.end(data);
+    });
+
 });
 
 server.listen(8080);
